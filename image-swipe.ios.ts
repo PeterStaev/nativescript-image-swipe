ff90ef7e3429063578b1ad0ad364e52d735c2836
Erik Telford <erik@microventures.com>
2019-04-18 08:16:58 -0500

    Allow for Safe Area Insets for iPhone X, etc.

--- image-swipe.ios.ts
+++ image-swipe.ios.ts
@@ -299,9 +299,12 @@ export class ImageSwipe extends ImageSwipeBase {
         const scrollView: UIScrollView = this.ios;
         const width = utils.layout.toDeviceIndependentPixels(this.getMeasuredWidth());
         const height = utils.layout.toDeviceIndependentPixels(this.getMeasuredHeight());
+        const safeAreaInsets = this.getSafeAreaInsets();
+        const insetAllowance = utils.layout.toDeviceIndependentPixels(safeAreaInsets.left) * 2;
+        const calculatedWidth = width + insetAllowance;
 
-        // console.log(`_calcScrollViewContentSize ${width}, ${height}`);
-        scrollView.contentSize = CGSizeMake(this.items.length * width, height);
+        // console.log(`_calcScrollViewContentSize ${calculatedWidth}, ${height}`);
+        scrollView.contentSize = CGSizeMake(this.items.length * calculatedWidth, height);
     }
 }
 
